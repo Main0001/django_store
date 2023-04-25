add3d19196816af13ff601b8d34aa4daee91d632
Maxim Mushynski <makmushinskiy@inbox.ru>
2023-04-25 15:51:30 +0300

    add tests for user registration

--- store-server/store/users/tests.py
+++ store-server/store/users/tests.py
@@ -1,7 +1,10 @@
 from django.test import TestCase
 from django.urls import reverse
+from django.utils.timezone import now
 
-from users.models import User
+from datetime import timedelta
+
+from users.models import User, EmailVerification
 
 from users.forms import UserRegistrationForm
 
@@ -20,7 +23,6 @@ class UserRegistrationViewTestCase(TestCase):
                 'password2': 'Mm_password1'
         }
 
-
     def test_user_registration_get(self):
         response = self.client.get(self.path)
 
@@ -28,14 +30,27 @@ class UserRegistrationViewTestCase(TestCase):
         self.assertEqual(response.context_data['title'], 'Store - Регистрация')
         self.assertTemplateUsed(response, 'users/registration.html')
     
-    def test_user_registration_post_success(self):
-        data =                 
-        username = data['username']
+    def test_user_registration_post_success(self):            
+        username = self.data['username']
 
         self.assertFalse(User.objects.filter(username=username).exists())
-        response = self.client.post(self.path, data)
+        response = self.client.post(self.path, self.data)
 
         self.assertEqual(response.status_code, HTTPStatus.FOUND)
         self.assertRedirects(response, reverse('users:login'))
         self.assertTrue(User.objects.filter(username=username).exists())
 
+
+        email_verification = EmailVerification.objects.filter(user__username=username)
+        self.assertTrue(email_verification.exists())
+        self.assertEqual(
+            email_verification.first().expiration.date(),
+            (now() + timedelta(hours=48)).date()
+        )
+
+    def test_user_registration_post_error(self):
+        user = User.objects.create(username=self.data['username'])
+        response = self.client.post(self.path, self.data)
+
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertContains(response, 'Пользователь с таким именем уже существует.', html=True)
