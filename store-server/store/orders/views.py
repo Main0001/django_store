aa30bd113b2fe73ad73bad47b89a073e21f8f90e
Maxim Mushynski <makmushinskiy@inbox.ru>
2023-07-02 17:31:46 +0300

    add orders templates and logic

--- store-server/store/orders/views.py
+++ store-server/store/orders/views.py
@@ -1,6 +1,19 @@
-from django.views.generic.base import TemplateView
+from django.forms.models import BaseModelForm
+from django.http import HttpResponse
 from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
+
+from orders.forms import OrderForm
+
+from common.views import TitleMixin
 
 # Create your views here.
-class OrderCreateView(TemplateView):
+class OrderCreateView(TitleMixin, CreateView):
     template_name = 'orders/order-create.html'
+    form_class = OrderForm
+    success_url = reverse_lazy('orders:order_create')
+    title = 'Store - оформление заказа'
+
+    def form_valid(self, form):
+        form.instance.initiator = self.request.user
+        return super(OrderCreateView, self).form_valid(form)
